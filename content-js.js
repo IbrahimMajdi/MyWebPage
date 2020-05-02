177ce2ccc26a643cc6608091ea455a9079d77bdd
IbrahimMajdi <ibrmaj1@gmail.com>
2020-05-02 06:28:31 -0700

    refactored the code to use more clear functions

--- content-js.js
+++ content-js.js
@@ -1,20 +1,18 @@
 function nameFunction() {
 
-        var name = prompt ('Hi there, nice to see you on our site. What should we call you? :)');
+  var name = prompt('Hi there, nice to see you on our site. What should we call you? :)');
 
-        if (name != null) {                  
+  var contact = confirm(' would you like us to contact you for help? :)');
 
-        document.getElementById("name").innerHTML ="Hello " + name + "! How are you today?";
+  if (contact == true) {
+    alert("Thanks! will contact you ASAP :)");
+  } else {
+    alert("Feel free to contact us any time :)");;
+  }
 
-        var contact = confirm (' would you like us to contact you for help? :)');
-        
-        if (contact == true) {
-           alert("Thanks! will contact you ASAP :)") ;
-          } else {
-            alert("Feel free to contact us any time :)") ;;
-          }
-        
+  if (name != null) {
+    //document.getElementById("name").innerHTML ="Hello " + name + "! How are you today?";
+    return "Hello " + name + "! How are you today?";
 
-        }                                  
+  }
 }
-
