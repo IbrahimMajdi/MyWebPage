e7a301dc4a4ca29b53128f1e2726e958cb9ec243
IbrahimMajdi <ibrmaj1@gmail.com>
2020-05-03 02:11:35 -0700

    lab:08

--- content-js.js
+++ content-js.js
@@ -1,18 +1,39 @@
+var employees;
+
 function nameFunction() {
 
-  var name = prompt('Hi there, nice to see you on our site. What should we call you? :)');
+  var name = prompt('Hi there, nice to see you on our site. What should we call you?:)');
 
-  var contact = confirm(' would you like us to contact you for help? :)');
+  if (name == String || name != "") {
 
-  if (contact == true) {
-    alert("Thanks! will contact you ASAP :)");
-  } else {
-    alert("Feel free to contact us any time :)");;
-  }
+    var contact = confirm(' would you like us to contact you for help? :)');
 
-  if (name != null) {
-    //document.getElementById("name").innerHTML ="Hello " + name + "! How are you today?";
+    if (contact == true) {
+      alert("Thanks! will contact you ASAP :)");
+    } else {
+      alert("Feel free to contact us any time :)");
+    }
+
     return "Hello " + name + "! How are you today?";
 
+  } else {
+
+    alert("Name must be filled out");
   }
+
+
+}
+
+function icons() {
+
+  var icon=1;
+
+  employees = prompt('how many employees are in your company?:)');
+
+  for (var i = 0; i < employees; i++) {
+
+    icon = icon +'<img src ="images/user.png"/>';
+
+  }
+  return icon;
 }
